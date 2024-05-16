6a81924af8f37eb2e8c2f1a36ffc4b6d944ec188
AllenCook <albcook1313@hotmail.com>
2024-05-15 17:54:32 -0700

    finish repo

--- hash-table.js
+++ hash-table.js
@@ -9,31 +9,62 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
-    // Your code here 
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
   }
 
   hash(key) {
-    // Your code here 
+    return parseInt(sha256(key).slice(0, 8), 16);
   }
 
   hashMod(key) {
-    // Your code here 
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here 
+    let ind = this.hashMod(key);
+    let newPair = new KeyValuePair(key, value);
+    if(!this.data[ind]) {
+      this.data[ind] = newPair;
+      this.count++;
+    } else {
+      throw new Error('hash collision or same key/value pair already exists!')
+    }
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here 
+    let ind = this.hashMod(key);
+    let newPair = new KeyValuePair(key, value);
+    if(!this.data[ind]) {
+      this.data[ind] = newPair;
+    } else {
+      newPair.next = this.data[ind];
+      this.data[ind] = newPair;
+    }
+    this.count++;
   }
 
   insert(key, value) {
-    // Your code here 
+    let ind = this.hashMod(key);
+    let cur = this.data[ind];
+    while(cur && cur.key !== key) {
+      cur = cur.next;
+    }
+    if(cur) {
+      cur.value = value;
+    } else {
+      let newPair = new KeyValuePair(key, value);
+      if(!this.data[ind]) {
+        this.data[ind] = newPair;
+      } else {
+        newPair.next = this.data[ind];
+        this.data[ind] = newPair;
+      }
+      this.count++
+    }
   }
-
 }
 
 
